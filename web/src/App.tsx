073ab63e0683d8ff9f0ef8e7e5028c5acb2bb753
Paulo Mendes <paulo.h.mendes25@gmail.com>
2022-09-13 16:18:22 -0300

    feat: adding and configurating tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import './styles/main.css';
 
 function App() {
   return (
-    <h1>{`Teste`}</h1>
+    <h1 className='text-fuchsia-400 bg-slate-400 text-7xl'>{`Teste`}</h1>
   )
 }
 
