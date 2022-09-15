909c5ba756646ff9ecfdba9d0b2efe94384bf311
Paulo Mendes <paulo.h.mendes25@gmail.com>
2022-09-15 02:47:06 -0300

    feat: title home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
+import { Logo } from './assets/Logo';
 import './styles/main.css';
 
 function App() {
   return (
-    <h1 className='text-fuchsia-400 bg-slate-400 text-7xl'>{`Teste`}</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center pt-20 gap-20'>
+      <Logo />
+      <div>
+        <h1 className='text-[64px] font-black text-primary-white'>{`Seu`} <span className='bg-gradient bg-clip-text text-transparent'>{`duo`}</span> {`está aqui.`}</h1>
+      </div>
+    </div>
   )
 }
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('./src/styles/colors.cjs')
+
 module.exports = {
-  content: ["./src/**/*.{tsx, jsx}"],
+  content: [
+    './src/**/*.{tsx, jsx}',
+    './index.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors,
+      backgroundImage: {
+        galaxy: "url('/fundo.png')",
+        gradient: 'linear-gradient(45deg, #9572FC 5.00%, #43E7AD 80%, #E1D55D 15%)'
+      }
+    },
   },
   plugins: [],
 }
